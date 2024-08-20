77342161b6f9901c398ffb7dfe52a3394f633cf8
Oleksandr Berezhnyi <berrezhnoy@gmail.com>
2024-08-20 04:07:15 +0300

    Create basic routes and responsive menu

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+import Menu from "@/app/components/Menu";
+
+export default function DashboardLayout({
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className="h-screen flex">
+            <div className="w-[14%] md:w[8%] lg-[16%] xl:w-[14%] p-4">
+                <Link className={"flex items-center justify-center lg:justify-start gap-2"} href="/">
+                    <Image src="/logo.png" alt="logo" width={32} height={32} />
+                    <span className={"hidden lg:block"}>Schoolio</span>
+                </Link>
+                <Menu />
+            </div>
+            <div className="w-[86%] md:[92%] lg-[84%] xl:w-[86%] p-4">RIGHT</div>
+        </div>
+    );
+}
--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const menuItems = [
     {
         title: "MENU",
@@ -112,3 +115,23 @@ const menuItems = [
         ],
     },
 ];
+
+const Menu = () => {
+  return (
+      <div className={"mt-4 text-sm"}>
+          {menuItems.map(item => (
+              <div className={"flex flex-col gap-2"} key={item.title}>
+                  <span className={"hidden lg:block text-gray-400 font-light my-4"}>{item.title}</span>
+                  {item.items.map(menuItem => (
+                      <Link className={"flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"} href={menuItem.href} key={menuItem.label}>
+                          <Image src={menuItem.icon} alt={menuItem.label} width={20} height={20}/>
+                          <span className={"hidden lg:block"}>{menuItem.label}</span>
+                      </Link>
+                  ))}
+              </div>
+          ))}
+      </div>
+  )
+}
+
+export default  Menu;
